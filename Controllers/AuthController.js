f4d2e1bf1c472fdbc43422adb196586ecaec6178
Muhammad Silmi Kaffah 22 <simi.prambos@gmail.com>
2019-06-27 22:17:03 +0700

    fix bug

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -5,49 +5,46 @@ const User = models.user
 
 class AuthController {
 
-    static async login(req, res) {
-        let { username, password } = req.body
+	static async login(req, res) {
+		let { username, password } = req.body
 
-        await User.findOne({ where: { username, password } })
-            .then(user => {
-                if (user) {
-                    let jwt = JwtAuthMiddleware.signJwt({ id: user.id, email: user.email })
-					res.status(200).send({ email: user.email, jwt })
-					return
-                }
-                res.status(401).send({detail: 'Wrong username or password'})
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }
+		const user = await User.findOne({ where: { username } })
+		if (!user) {
+			return res.status(401).send({ message: 'not_registered' })
+		}
+		if (user && user.password !== password) {
+			return res.status(401).send({ message: 'wrong_password' })
+		}
 
-    // static async logout(req, res) {
+		let jwt = JwtAuthMiddleware.signJwt({ id: user.id, email: user.email })
+		res.status(200).send({ email: user.email, jwt })
+	}
 
-    //     let {token, type} = JwtAuthMiddleware.extractToken(req.headers)
-    //     await Token.create({jwt: token, blacklisted: true})
-    //         .then(blacklistedToken => {
-    //             console.log(blacklistedToken)
-    //             res.status(200).send({})
-    //         })
-    //         .catch(error => {
-    //             console.log('create token error')
-    //             // res.status(500).send({})
-    //         })
+	// static async logout(req, res) {
 
-    // }
+	//     let {token, type} = JwtAuthMiddleware.extractToken(req.headers)
+	//     await Token.create({jwt: token, blacklisted: true})
+	//         .then(blacklistedToken => {
+	//             console.log(blacklistedToken)
+	//             res.status(200).send({})
+	//         })
+	//         .catch(error => {
+	//             console.log('create token error')
+	//             // res.status(500).send({})
+	//         })
 
-    static async register(req, res) {
-        await User.create(req.body)
-            .then(user => {
-                let { id, username, email, createdAt } = user
-                res.status(201).send({ id, username, email, createdAt })
-            })
-            .catch(err => {
-				console.log(err)
-                res.status(500).send('Internal server error')
-            })
-    }
+	// }
+
+	static async register(req, res) {
+		await User.create(req.body)
+			.then(user => {
+				let { id, username, email, createdAt } = user
+				res.status(201).send({ id, username, email, createdAt })
+			})
+			.catch(err => {
+				res.status(500).send(err)
+			})
+	}
 }
 
 module.exports = AuthController
